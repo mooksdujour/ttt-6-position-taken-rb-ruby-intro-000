af5e1e4df89901c8d136c4d293f12d07347b06be
John Moo-King <jmooking@hotmail.com>
2019-05-07 22:35:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def board_taken?(board, index)
+  if board[index] = " " || board[index] = "" || board[index = nil]
+    false
+  else
+    true
+  end
+end
+  
